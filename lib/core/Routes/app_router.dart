c5088c4289b44f0eb7c31cfb7a7bca477e4c8efa
C0de871 <muhammadaydi3@gmail.com>
2024-11-30 13:27:33 +0300

    add app localization packages and setting them

--- lib/core/Routes/app_router.dart
+++ lib/core/Routes/app_router.dart
@@ -1,11 +1,12 @@
 import 'package:e_commerce/core/constants/app_routes.dart';
 import 'package:flutter/material.dart';
+import '../../features/user/presentation/sign up general info screen/sign_up_general_info_screen.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      // case AppRoutes.signUpGeneralInfoRoute:
-      //   return MaterialPageRoute(builder: (_) => SignUpGeneralInfo());
+      case AppRoutes.signUpGeneralInfoRoute:
+        return MaterialPageRoute(builder: (_) => const SignUpGeneralInfoScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,9 +1,13 @@
 
-import 'package:e_commerce/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:e_commerce/core/theme/app_theme.dart';
 
 import '../Routes/app_router.dart';
 import '../constants/app_routes.dart';
+import '../l10n/l10n.dart';
 import '../theme/theme_manager.dart';
 
 ThemeManager _themeManager = ThemeManager();
@@ -15,6 +19,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: L10n.all,
+      locale: const Locale('ar'),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
       debugShowCheckedModeBanner: false,
       title: 'E-Commerce',
       theme: AppTheme.lightTheme,
