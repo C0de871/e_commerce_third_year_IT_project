4b1aaf215965fbd87c1030d189a24e8f1c55b6ff
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-04 15:23:14 +0300

    finish model bu user and sub user and rename User class to

---
+++ lib/features/user/data/models/sub_models/sub_user_model.dart
@@ -0,0 +1,32 @@
+import 'package:collection/collection.dart';
+import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/features/user/domain/entites/user_entities/user.dart';
+
+class SubUserModel extends SubUserEntity {
+
+  SubUserModel({super.id, super.name, super.email});
+
+  factory SubUserModel.fromJson(Map<String, dynamic> json) => SubUserModel(
+        id: json[ApiKey.id] ,
+        name: json[ApiKey.name] ,
+        email: json[ApiKey.email] ,
+      );
+
+  Map<String, dynamic> toJson() => {
+        ApiKey.id: id,
+        ApiKey.name: name,
+        ApiKey.email: email,
+      };
+
+  // @override
+//   bool operator ==(Object other) {
+//     if (identical(other, this)) return true;
+//     if (other is! User) return false;
+//     final mapEquals = const DeepCollectionEquality().equals;
+//     return mapEquals(other.toJson(), toJson());
+//   }
+
+//   @override
+//   int get hashCode => id.hashCode ^ name.hashCode ^ email.hashCode;
+// 
+}
---
+++ lib/features/user/data/models/user_model.dart
@@ -0,0 +1,34 @@
+import 'package:collection/collection.dart';
+import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/features/user/domain/entites/user_entities/user_entities.dart';
+
+import 'sub_models/sub_user_model.dart';
+
+class UserModel  extends UserEntity {
+  
+  UserModel({required super.accessToken, super.refreshToken, super.user});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        accessToken: json[ApiKey.accessToken],
+        refreshToken: json[ApiKey.refreshToken],
+        user: SubUserModel.fromJson(json[ApiKey.user])
+      );
+
+  Map<String, dynamic> toJson() => {
+        ApiKey.accessToken: accessToken,
+        ApiKey.refreshToken: refreshToken,
+        ApiKey.user: user
+      };
+
+  // @override
+  // bool operator ==(Object other) {
+  //   if (identical(other, this)) return true;
+  //   if (other is! UserModel) return false;
+  //   final mapEquals = const DeepCollectionEquality().equals;
+  //   return mapEquals(other.toJson(), toJson());
+  // }
+
+  // @override
+  // int get hashCode =>
+  //     accessToken.hashCode ^ refreshToken.hashCode ^ user.hashCode;
+}
