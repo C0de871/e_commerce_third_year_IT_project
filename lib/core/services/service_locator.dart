fc09aeca4b338d429d7fabf2051c5e7af9c31dac
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-04 22:03:27 +0300

    مشان الحقن ما نعرف كتير اوبجيكتات

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,7 +1,10 @@
-
-
 import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:dio/dio.dart';
+import 'package:e_commerce/features/user/data/datasourses/user_local_data_source.dart';
+import 'package:e_commerce/features/user/data/datasourses/user_remote_data_source.dart';
+import 'package:e_commerce/features/user/data/repositiries/user_repository_impl.dart';
+import 'package:e_commerce/features/user/domain/repository/user_repository.dart';
+import 'package:e_commerce/features/user/domain/usecases/login_user.dart';
 import 'package:get_it/get_it.dart';
 
 import '../connection/network_info.dart';
@@ -15,21 +18,22 @@ void setupServicesLocator() {
   // Core
   getIt.registerLazySingleton<Dio>(() => Dio());
   getIt.registerLazySingleton<ApiConsumer>(() => DioConsumer(dio: getIt()));
-  getIt.registerLazySingleton<DataConnectionChecker>(() => DataConnectionChecker());
+  getIt.registerLazySingleton<DataConnectionChecker>(
+      () => DataConnectionChecker());
   getIt.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(getIt()));
   getIt.registerLazySingleton<CacheHelper>(() => CacheHelper());
 
   // Data Sources
-  // getIt.registerLazySingleton<UserRemoteDataSource>(() => UserRemoteDataSource(api: getIt()));
-  // getIt.registerLazySingleton<UserLocalDataSource>(() => UserLocalDataSource(cache: getIt()));
+  getIt.registerLazySingleton<UserRemoteDataSource>(() =>UserRemoteDataSource (api: getIt()));
+  getIt.registerLazySingleton<UserLocalDataSource>(() => UserLocalDataSource(cache: getIt()));
 
   // Repository
-  // getIt.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(
-  //       networkInfo: getIt(),
-  //       remoteDataSource: getIt(),
-  //       localDataSource: getIt(),
-  //     ));
+  getIt.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(
+      networkInfo: getIt(),
+        remoteDataSource: getIt(),
+        localDataSource: getIt(),
+      ));
 
   // Use Cases
-  // getIt.registerLazySingleton<GetUser>(() => GetUser(repository: getIt()));
-}
+  getIt.registerLazySingleton<LoginUser>(() => LoginUser(userRepository: getIt()));
+} 
