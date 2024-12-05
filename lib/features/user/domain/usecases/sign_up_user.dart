8188418d2a05b895a04236c37d9086119408861e
C0de871 <muhammadaydi3@gmail.com>
2024-12-05 21:58:55 +0300

    add sign up usecase

--- lib/features/user/domain/usecases/sign_up_user.dart
+++ lib/features/user/domain/usecases/sign_up_user.dart
@@ -3,11 +3,10 @@ import 'package:e_commerce/core/errors/failure.dart';
 import 'package:e_commerce/features/user/domain/entites/user_entities/user_entities.dart';
 import 'package:e_commerce/features/user/domain/repository/user_repository.dart';
 
-class LoginUser {
-  final UserRepository userRepository;
-
-  LoginUser({required this.userRepository});
-  Future<Either<Failure, UserEntity>> call({required Map<String, dynamic> bodyjson}) {
-    return userRepository.loginUser(bodyjson: bodyjson);
+class SignUpUser {
+  UserRepository userRepository;
+  SignUpUser({required this.userRepository});
+  Future<Either<Failure, UserEntity>> call({required Map<String, dynamic> jsonBody}) {
+    return userRepository.signUpUser(jsonBody: jsonBody);
   }
 }
