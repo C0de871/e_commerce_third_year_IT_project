9a83749d1f3e260e08618f391324527cecd9df82
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-05 19:54:54 +0300

    delete import (js) that makes errors

--- lib/features/user/data/models/sub_models/sub_user_model.dart
+++ lib/features/user/data/models/sub_models/sub_user_model.dart
@@ -1,3 +1,4 @@
+// ignore: unused_import
 import 'package:collection/collection.dart';
 import 'package:e_commerce/core/databases/api/end_points.dart';
 import 'package:e_commerce/features/user/domain/entites/user_entities/sub_user_entity.dart';
