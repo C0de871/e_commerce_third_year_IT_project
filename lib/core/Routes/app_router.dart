881427213a219e14bcbd37287d4ec25e374aff23
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-05 20:03:16 +0300

    add import for cubit & login screen and add bloc provider to login route

--- lib/core/Routes/app_router.dart
+++ lib/core/Routes/app_router.dart
@@ -1,7 +1,10 @@
 import 'package:e_commerce/core/constants/app_routes.dart';
 import 'package:e_commerce/features/user/presentation/OTP/otp_screen.dart';
+import 'package:e_commerce/features/user/presentation/cubit/user_cubit.dart';
+import 'package:e_commerce/features/user/presentation/login_screen/logInScreen.dart';
 import 'package:e_commerce/features/user/presentation/splash%20screen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../features/user/presentation/complete_profile_screen/complete_profile_screen.dart';
 import '../../features/user/presentation/sign up auth screen/sign_up_auth_screen.dart';
 
@@ -25,10 +28,16 @@ class AppRouter {
           ),
         );
 
-      case AppRoutes.splashScreen:
-        return MaterialPageRoute(builder: (_) => SplashScreen());
-      // case AppRoutes.signUpGeneralInfoRoute:
-      //   return MaterialPageRoute(builder: (_) => const SignUpGeneralInfoScreen());
+      case AppRoutes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case AppRoutes.loginRoute:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => UserCubit(),
+            child: const LoginScreen(),
+          ),
+        );
+
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
