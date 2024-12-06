247888ece0f79cb52b40d66820b9a7528cf88c7c
C0de871 <muhammadaydi3@gmail.com>
2024-12-06 16:51:04 +0300

    add signupuser method to user remote data source and add header to the login function

--- lib/features/user/data/datasourses/user_remote_data_source.dart
+++ lib/features/user/data/datasourses/user_remote_data_source.dart
@@ -1,16 +1,27 @@
 import 'package:e_commerce/core/databases/api/api_consumer.dart';
 import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/features/user/data/models/sign_up_model.dart';
 import 'package:e_commerce/features/user/data/models/user_model.dart';
 
 class UserRemoteDataSource {
-final ApiConsumer api;
+  final ApiConsumer api;
 
   UserRemoteDataSource({required this.api});
 
-Future<UserModel> loginUser(Map<String,dynamic> jsonbody) async{
-  final response=await api.post(EndPoints.login,data: jsonbody);
-  // print(response.)
+  Future<UserModel> loginUser(Map<String, dynamic> jsonbody) async {
+    Map<String, dynamic> headers = {
+      ApiKey.deviceId: 1,
+    };
+    final response = await api.post(
+      EndPoints.login,
+      data: jsonbody,
+      headers: headers,
+    );
     return UserModel.fromJson(response);
-// //todo body of request after login screen
-// return ;   }
-}}
+  }
+
+  Future<SignUpModel> signUpUser(Map<String, dynamic> jsonbody) async {
+    final response = await api.post(EndPoints.signUp, data: jsonbody);
+    return SignUpModel.fromJson(response);
+  }
+}
