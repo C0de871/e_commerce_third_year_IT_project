8ac9820ff6d568ca7cc7ab224500243042449dfc
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-07 13:33:37 +0300

    add images in app images

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,3 +1,6 @@
 class AppImages {
-  static const String splashImage = "assets/images/splash_1.png";
+  static const String splashImage1 = "assets/images/splash_1.png";
+  static const String splashImage2=  "assets/images/splash_2.png";
+  static const String splashImage3=  "assets/images/splash_3.png";
+
 }
