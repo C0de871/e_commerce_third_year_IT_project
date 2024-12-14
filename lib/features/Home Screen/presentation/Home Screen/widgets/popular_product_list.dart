78e3ce47f89f086ead9856678c2378003a22271b
C0de871 <muhammadaydi3@gmail.com>
2024-12-14 18:12:24 +0300

    popular products list

---
+++ lib/features/Home Screen/presentation/Home Screen/widgets/popular_product_list.dart
@@ -0,0 +1,39 @@
+import 'package:e_commerce/core/constants/app_numbers.dart';
+import 'package:e_commerce/features/Home%20Screen/presentation/Home%20Screen/widgets/product_card.dart';
+import 'package:flutter/material.dart';
+
+class PopularProductList extends StatelessWidget {
+  const PopularProductList({
+    super.key,
+    required this.fetchingProductState,
+  });
+
+  final FetchProductState fetchingProductState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: padding4 * 4,
+      ),
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.only(bottom: 20),
+        scrollDirection: Axis.horizontal,
+        //todo: remove every fetchingproduct state:
+        child: Row(
+          children: [
+            ProductCard(fetchProductState: fetchingProductState),
+            const SizedBox(
+              width: padding4 * 4,
+            ),
+            ProductCard(fetchProductState: fetchingProductState),
+            const SizedBox(
+              width: padding4 * 4,
+            ),
+            ProductCard(fetchProductState: fetchingProductState),
+          ],
+        ),
+      ),
+    );
+  }
+}
