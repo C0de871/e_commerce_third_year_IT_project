ba4ed171fc810f44df3d2927847aa63812c422d0
C0de871 <muhammadaydi3@gmail.com>
2024-12-14 18:12:11 +0300

    refactoring

--- lib/features/Home Screen/presentation/Home Screen/widgets/product_card.dart
+++ lib/features/Home Screen/presentation/Home Screen/widgets/product_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
