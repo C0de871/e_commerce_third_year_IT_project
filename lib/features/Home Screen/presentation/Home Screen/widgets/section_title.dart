2288510076829b9a8faa11fbe95456221e7ecda6
C0de871 <muhammadaydi3@gmail.com>
2024-12-14 18:05:47 +0300

    section title

---
+++ lib/features/Home Screen/presentation/Home Screen/widgets/section_title.dart
@@ -0,0 +1,47 @@
+import 'package:e_commerce/core/constants/app_numbers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Import the generated localization class
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    super.key,
+    required this.text,
+    required this.press,
+  });
+
+  final String text;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    String seeMoreText = AppLocalizations.of(context)!.seeMore;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: padding4 * 4,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 18,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+          GestureDetector(
+            onTap: press,
+            child: Text(
+              seeMoreText, // Use the localized "See More" text
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
