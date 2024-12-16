cd8e70e341116cf98a1c40f2d9866fc5449a9585
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-17 01:28:22 +0300

    add otp functions to repository

--- lib/features/user/domain/repository/user_repository.dart
+++ lib/features/user/domain/repository/user_repository.dart
@@ -3,11 +3,20 @@ import 'package:e_commerce/core/errors/failure.dart';
 import 'package:e_commerce/features/user/domain/entites/Sign%20up%20entities/sign_up_entity.dart';
 import 'package:e_commerce/features/user/domain/entites/user_entities/user_entities.dart';
 
+import '../entites/otp_entities/otp_entity.dart';
+
 abstract class UserRepository {
   Future<Either<Failure, UserEntity>> loginUser({
     required Map<String, dynamic> bodyjson,
   });
   Future<Either<Failure, SignUpEntity>> signUpUser({
     required Map<String, dynamic> jsonBody,
+  });
+
+  Future<Either<Failure, OtpEntity>> resendOtp({
+    required Map<String, dynamic> bodyjson,
+  });
+  Future<Either<Failure, OtpEntity>> postOtp({
+    required Map<String, dynamic> bodyjson,
   });
 }
