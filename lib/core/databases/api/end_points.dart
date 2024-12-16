c61347e3a764aac9235e6e3dac6d6cf1acd006f7
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-17 01:33:17 +0300

    change base url and add otp string

--- lib/core/databases/api/end_points.dart
+++ lib/core/databases/api/end_points.dart
@@ -1,11 +1,14 @@
 class EndPoints {
-  static const String baserUrl = "http://192.168.1.5:8000/api/";
+  static const String baserUrl = "http://10.65.0.102:8000/api/";
   static const String login = "login";
   static const String signUp = "register";
   static const String template = "template/";
+  static const String otpResend = "resend-otp";
+  static const String otpValidate = "validate-otp";
 }
 
 class ApiKey {
+  static String otp="otp";
   static String id = "id";
   static String message = "message";
   static String statusCode = "status_code";
@@ -16,8 +19,8 @@ class ApiKey {
   static String user = "user";
   static String password = "password";
   static String deviceId = "Device-ID";
-  static String firstName ="first_name";
-  static String lastName="last_name";
-  static String phoneNumber="phone_number";
-  static String address="location";
+  static String firstName = "first_name";
+  static String lastName = "last_name";
+  static String phoneNumber = "phone_number";
+  static String address = "location";
 }
