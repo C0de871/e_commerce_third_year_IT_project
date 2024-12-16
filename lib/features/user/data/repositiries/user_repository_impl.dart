48de95cfb0ff556f20ecc004f5e5b0725e0dcbe5
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-17 01:30:40 +0300

    add otp function to repo impl to do them

--- lib/features/user/data/repositiries/user_repository_impl.dart
+++ lib/features/user/data/repositiries/user_repository_impl.dart
@@ -5,6 +5,7 @@ import 'package:e_commerce/core/errors/failure.dart';
 import 'package:e_commerce/features/user/data/datasourses/user_local_data_source.dart';
 import 'package:e_commerce/features/user/data/datasourses/user_remote_data_source.dart';
 import 'package:e_commerce/features/user/domain/entites/Sign%20up%20entities/sign_up_entity.dart';
+import 'package:e_commerce/features/user/domain/entites/otp_entities/otp_entity.dart';
 import 'package:e_commerce/features/user/domain/entites/user_entities/user_entities.dart';
 import 'package:e_commerce/features/user/domain/repository/user_repository.dart';
 
@@ -13,7 +14,10 @@ class UserRepositoryImpl extends UserRepository {
   final UserRemoteDataSource remoteDataSource;
   final UserLocalDataSource localDataSource;
 
-  UserRepositoryImpl({required this.remoteDataSource, required this.localDataSource, required this.networkInfo});
+  UserRepositoryImpl(
+      {required this.remoteDataSource,
+      required this.localDataSource,
+      required this.networkInfo});
   @override
   Future<Either<Failure, UserEntity>> loginUser({
     required Map<String, dynamic> bodyjson,
@@ -37,11 +41,52 @@ class UserRepositoryImpl extends UserRepository {
   }
 
   @override
-  Future<Either<Failure, SignUpEntity>> signUpUser({required Map<String, dynamic> jsonBody}) async {
+  Future<Either<Failure, SignUpEntity>> signUpUser(
+      {required Map<String, dynamic> jsonBody}) async {
     if (await networkInfo.isConnected!) {
       try {
         final remoteSignUpModel = await remoteDataSource.signUpUser(jsonBody);
         return Right(remoteSignUpModel);
+      } on ServerException catch (e) {
+        return Left(Failure(errMessage: e.errorModel.errorMessage));
+      }
+    } else {
+      return Left(
+        Failure(
+          errMessage: localDataSource.noInternectConnection(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, OtpEntity>> resendOtp({
+    required Map<String, dynamic> bodyjson,
+  }) async {
+    if (await networkInfo.isConnected!) {
+      try {
+        final remoteResendOtp = await remoteDataSource.resendOtp(bodyjson);
+        return Right(remoteResendOtp);
+      } on ServerException catch (e) {
+        return Left(Failure(errMessage: e.errorModel.errorMessage));
+      }
+    } else {
+      return Left(
+        Failure(
+          errMessage: localDataSource.noInternectConnection(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, OtpEntity>> postOtp({
+    required Map<String, dynamic> bodyjson,
+  }) async {
+    if (await networkInfo.isConnected!) {
+      try {
+        final remoteResendOtp = await remoteDataSource.postOtp(bodyjson);
+        return Right(remoteResendOtp);
       } on ServerException catch (e) {
         return Left(Failure(errMessage: e.errorModel.errorMessage));
       }
