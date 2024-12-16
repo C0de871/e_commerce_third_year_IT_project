c16ac076d2da714d896ec30c9390d331f17bb6ef
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-17 01:27:41 +0300

    create new use case (post otp and validate it) complettly

---
+++ lib/features/user/domain/usecases/post_otp.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/errors/failure.dart';
+import 'package:e_commerce/features/user/domain/entites/otp_entities/otp_entity.dart';
+import 'package:e_commerce/features/user/domain/repository/user_repository.dart';
+
+class PostOtp {
+  final UserRepository userRepository;
+
+  PostOtp({required this.userRepository});
+
+  Future<Either<Failure, OtpEntity>> call({
+    required Map<String, dynamic> bodyjson,
+  }) {
+    return userRepository.postOtp(bodyjson: bodyjson);
+  }
+}
