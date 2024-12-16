dcfb98c81dc75c47bb771f33348d8c8ea5e7acb2
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-17 01:30:11 +0300

    add dio function resend otp and post it by user remote

--- lib/features/user/data/datasourses/user_remote_data_source.dart
+++ lib/features/user/data/datasourses/user_remote_data_source.dart
@@ -1,7 +1,12 @@
+import 'package:dartz/dartz.dart';
 import 'package:e_commerce/core/databases/api/api_consumer.dart';
 import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/core/errors/failure.dart';
+import 'package:e_commerce/features/user/data/models/otp_model.dart';
 import 'package:e_commerce/features/user/data/models/sign_up_model.dart';
 import 'package:e_commerce/features/user/data/models/user_model.dart';
+import 'package:e_commerce/features/user/domain/entites/otp_entities/otp_entity.dart';
+import 'package:e_commerce/features/user/domain/usecases/resend_otp.dart';
 
 class UserRemoteDataSource {
   final ApiConsumer api;
@@ -24,7 +29,8 @@ class UserRemoteDataSource {
     Map<String, dynamic> headers = {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Cookie': 'just_go_deliver_app_session=ZVV1HxCQriLIQRYZZzHqh7Xw3wT2YeW1iQiIpb9q',
+      'Cookie':
+          'just_go_deliver_app_session=ZVV1HxCQriLIQRYZZzHqh7Xw3wT2YeW1iQiIpb9q',
     };
     final response = await api.post(
       EndPoints.signUp,
@@ -32,5 +38,23 @@ class UserRemoteDataSource {
       headers: headers,
     );
     return SignUpModel.fromJson(response);
+  }
+
+  Future<OtpModel> resendOtp(
+      Map<String, dynamic> jsonbody) async {
+    final response = await api.post(
+      EndPoints.otpResend,
+      data: jsonbody,
+    );
+    return OtpModel.fromJson(response);
+  }
+
+  Future<OtpModel> postOtp(
+      Map<String, dynamic> jsonbody) async {
+    final response = await api.post(
+      EndPoints.otpValidate,
+      data: jsonbody,
+    );
+    return OtpModel.fromJson(response);
   }
 }
