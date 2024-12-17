f96e2b69f6a50168f1fdcda32b7e24987b6ac334
C0de871 <muhammadaydi3@gmail.com>
2024-12-17 20:09:51 +0300

    add pick image function

--- lib/core/functions/app_functions.dart
+++ lib/core/functions/app_functions.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 SystemUiOverlayStyle getSystemUiOverlayStyle(Brightness currentBrightness, BuildContext context) {
   return currentBrightness == Brightness.light
@@ -9,4 +13,11 @@ SystemUiOverlayStyle getSystemUiOverlayStyle(Brightness currentBrightness, Build
       : SystemUiOverlayStyle.dark.copyWith(
           statusBarColor: Theme.of(context).colorScheme.inversePrimary,
         );
+}
+
+Future<File?> pickImage() async {
+  final ImagePicker picker = ImagePicker();
+  final image = await picker.pickImage(source: ImageSource.gallery); // or ImageSource.camera
+  if (image == null) return null;
+  return File(image.path);
 }
