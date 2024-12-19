a6bdb5804772f2b84c44a6f0817a478f6512deae
C0de871 <muhammadaydi3@gmail.com>
2024-12-19 12:18:13 +0300

    abstract product repo

---
+++ lib/features/Home common/products feature/domain/repository/product_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/errors/failure.dart';
+import 'package:e_commerce/core/params/params.dart';
+import 'package:e_commerce/features/Home%20common/products%20feature/domain/entities/product_enitty.dart';
+import 'package:e_commerce/features/user/domain/entites/user_entities/user_entities.dart';
+
+abstract class ProductRepository {
+  Future<Either<Failure, List<ProductEntity>>> getAllProducts({required ProductParams params});
+}
