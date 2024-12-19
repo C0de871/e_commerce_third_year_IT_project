2a8b1f9771d4d8269fb737d83f32a47190fa7dcd
C0de871 <muhammadaydi3@gmail.com>
2024-12-19 12:16:53 +0300

    add fetch product parms

--- lib/core/params/params.dart
+++ lib/core/params/params.dart
@@ -1,4 +1,17 @@
+import 'package:e_commerce/core/databases/api/end_points.dart';
+
 class TemplateParams {
   final String id;
   TemplateParams({required this.id});
+}
+
+class ProductParams {
+  int page = 1;
+  ProductParams({required this.page});
+
+  Map<String, dynamic> toJson() {
+    return {
+      ApiKey.page: page,
+    };
+  }
 }
