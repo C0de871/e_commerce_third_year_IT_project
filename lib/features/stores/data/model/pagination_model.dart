6aee35d120fdaf8e04f6e7bc70a80c80ae2a2d8d
C0de871 <muhammadaydi3@gmail.com>
2024-12-21 17:16:19 +0300

    pagination and store models

---
+++ lib/features/stores/data/model/pagination_model.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/features/stores/domain/entities/pagination_entity.dart';
+
+class PaginationModel extends PaginationEntity {
+  PaginationModel({
+    required super.currentPage,
+    required super.totalPage,
+    required super.hasMorePage,
+  });
+
+  factory PaginationModel.fromJson(Map<String, dynamic> json) {
+    return PaginationModel(
+      currentPage: json[ApiKey.currentPage],
+      totalPage: json[ApiKey.totalPages],
+      hasMorePage: json[ApiKey.hasMorePage],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      ApiKey.currentPage: currentPage,
+      ApiKey.totalPages: currentPage,
+      ApiKey.hasMorePage: currentPage,
+    };
+  }
+}
---
+++ lib/features/stores/data/model/store_model.dart
@@ -0,0 +1,40 @@
+import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/features/stores/data/model/pagination_model.dart';
+import 'package:e_commerce/features/stores/domain/entities/store_entity.dart';
+
+class StoreModel extends StoreEntity {
+  StoreModel({
+    required super.id,
+    required super.managerName,
+    required super.storeName,
+    required super.imageUrl,
+    required super.location,
+    required super.description,
+    required super.pagination,
+  });
+
+  factory StoreModel.fromJson(Map<String, dynamic> json) {
+    return StoreModel(
+      id: json[ApiKey.id],
+      managerName: json[ApiKey.manager],
+      storeName: json[ApiKey.storeName],
+      imageUrl: json[ApiKey.imageUrl],
+      location: json[ApiKey.location],
+      description: json[ApiKey.description],
+      pagination: PaginationModel.fromJson(json[ApiKey.pagination]),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      ApiKey.id: id,
+      ApiKey.manager: managerName,
+      ApiKey.storeName: storeName,
+      ApiKey.imageUrl: imageUrl,
+      ApiKey.location: location,
+      ApiKey.description: description,
+      //! to json
+      ApiKey.pagination: (pagination as PaginationModel).toJson(),
+    };
+  }
+}
