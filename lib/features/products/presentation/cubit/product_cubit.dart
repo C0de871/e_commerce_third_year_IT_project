310f91c0f4542454e22f11ce8c23ad6085ac0a39
C0de871 <muhammadaydi3@gmail.com>
2024-12-21 13:21:37 +0300

    create cubit for products

---
+++ lib/features/products/presentation/cubit/product_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:e_commerce/core/params/params.dart';
+import 'package:e_commerce/core/services/service_locator.dart';
+import 'package:e_commerce/features/products/domain/use%20cases/get_all_products.dart';
+import 'package:meta/meta.dart';
+
+import 'package:e_commerce/features/products/domain/entities/product_enitty.dart';
+
+part 'product_state.dart';
+
+class ProductCubit extends Cubit<ProductState> {
+
+  //!initialize cubit:
+  GetAllProducts getAllProductsUseCase;
+  ProductCubit()
+      : getAllProductsUseCase = getIt<GetAllProducts>(),
+        super(ProductInitial());
+
+  //! get all products:
+  dynamic getAllProducts({int page = 1}) async {
+    emit(GetAllProductsLoading());
+    final response = await getAllProductsUseCase.call(params: ProductParams(page: page));
+    response.fold(
+      (failure) => emit(GetAllProductsFailed(errMessage: failure.errMessage)),
+      (productsList) => emit(GetAllProductsSuccess(productsList: productsList)),
+    );
+  }
+}
