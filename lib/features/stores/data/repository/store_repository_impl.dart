cfaa805e821986bd71f2685497a6a62d4f6fde9a
C0de871 <muhammadaydi3@gmail.com>
2024-12-21 18:17:42 +0300

    store repository impl

---
+++ lib/features/stores/data/repository/store_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/databases/connection/network_info.dart';
+import 'package:e_commerce/core/databases/errors/expentions.dart';
+import 'package:e_commerce/core/databases/errors/failure.dart';
+import 'package:e_commerce/core/databases/params/params.dart';
+import 'package:e_commerce/features/stores/data/dataSources/store_remote_data_source.dart';
+import 'package:e_commerce/features/stores/domain/entities/store_entity.dart';
+import 'package:e_commerce/features/stores/domain/repository/store_repository.dart';
+
+class StoreRepositoryImpl extends StoreRepository {
+  final NetworkInfo network;
+  final StoreRemoteDataSource remoteDataSource;
+
+  StoreRepositoryImpl({required this.network, required this.remoteDataSource});
+  @override
+  Future<Either<Failure, StoreEntity>> getAllStores({required StoreParams params}) async {
+    if (await network.isConnected!) {
+      try {
+        final remoteStores = await remoteDataSource.getAllStores(params: params);
+        return Right(remoteStores);
+      } on ServerException catch (e) {
+        return Left(Failure(errMessage: e.errorModel.errorMessage));
+      }
+    } else {
+      //TODO make this message adapt to app language:
+      return Left(Failure(errMessage: "There is no internet connnect"));
+    }
+  }
+}
