d1bc4e040c1ad0edc30f0df295592f3e2adb2ff8
C0de871 <muhammadaydi3@gmail.com>
2024-12-24 01:07:13 +0300

    add some colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -3,5 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   static final gradientDarkGrey = const Color(0xff343434).withOpacity(0.4);
   static final gradientLightGrey = const Color(0xff343434).withOpacity(0.15);
-  static const favouriteColor = Color.fromARGB(255, 127, 200, 255);
+  static const grayColor = Color(0xFFACACAC);
+  static const black = Colors.black;
+  static const disableFavContainer = Color.fromARGB(255, 235, 234, 234);
+  static const disableFav = Color.fromARGB(255, 201, 200, 200);
+  static const imageBackground = Color.fromARGB(126, 230, 230, 230);
 }
