8744fd8537e2490324b65cb152a8f16b11646b52
C0de871 <muhammadaydi3@gmail.com>
2024-12-24 01:07:28 +0300

    image paths

--- lib/core/utils/constants/app_images.dart
+++ lib/core/utils/constants/app_images.dart
@@ -1,6 +1,9 @@
 class AppImages {
   static const String splashImage1 = "assets/images/splash_1.png";
-  static const String splashImage2=  "assets/images/splash_2.png";
-  static const String splashImage3=  "assets/images/splash_3.png";
-  static const String successImage= "assets/images/success.jpg";
+  static const String splashImage2 = "assets/images/splash_2.png";
+  static const String splashImage3 = "assets/images/splash_3.png";
+  static const String successImage = "assets/images/success.jpg";
+  static const String storeImagePlaceHolder =
+      "assets/images/Image Banner 2.png";
+  static const String tShirt = "assets/images/tshirt.png";
 }
