d0f6249e6bfad5b77846647d7432cc6110aa56d5
C0de871 <muhammadaydi3@gmail.com>
2024-12-24 00:44:30 +0300

    add refresh token to the user repo abstract

--- lib/features/user/domain/repository/user_repository.dart
+++ lib/features/user/domain/repository/user_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/core/databases/errors/failure.dart';
 import 'package:e_commerce/features/user/domain/entites/Sign%20up%20entities/sign_up_entity.dart';
+import 'package:e_commerce/features/user/domain/entites/refresh_token/refresh_token_entity.dart';
 import 'package:e_commerce/features/user/domain/entites/user_entities/user_entities.dart';
 
 import '../entites/otp_entities/otp_entity.dart';
@@ -19,4 +20,6 @@ abstract class UserRepository {
   Future<Either<Failure, OtpEntity>> postOtp({
     required Map<String, dynamic> bodyjson,
   });
+
+  Future<Either<Failure, RefreshTokenEntity>> refreshToken();
 }
