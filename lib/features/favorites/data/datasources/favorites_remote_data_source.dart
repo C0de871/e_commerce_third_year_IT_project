a4f16d8b1d03baba8e8bdc3fbdf8ed966bf49f30
C0de871 <muhammadaydi3@gmail.com>
2024-12-27 11:26:12 +0300

    favorite remote data source: toggle  fav on method

---
+++ lib/features/favorites/data/datasources/favorites_remote_data_source.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import '../../../../core/databases/api/api_consumer.dart';
+import '../../../../core/databases/api/end_points.dart';
+import '../../../../core/databases/cache/secure_storage_helper.dart';
+import '../../../../core/databases/params/params.dart';
+import '../models/toggle_fav_on_model.dart';
+
+class FavoritesRemoteDataSource {
+  final ApiConsumer api;
+  final SecureStorageHelper cacheHelper;
+  FavoritesRemoteDataSource({
+    required this.api,
+    required this.cacheHelper,
+  });
+  Future<ToggleFavOnModel> getToggleFavOn(ToggleFavOnParams params) async {
+    Map<String, dynamic> headers = {
+      ApiKey.authorization: await cacheHelper.getData(key: CacheKey.accessToken),
+    };
+
+    bool isLoggedIn = (await cacheHelper.getData(key: CacheKey.accessToken) != null) ? true : false;
+
+    Map<String, dynamic> extra = {
+      ApiKey.requiredAuth: isLoggedIn,
+    };
+    final response = await api.get(
+      "${EndPoints.toggleFavOn}/${params.id}",
+      headers: headers,
+      extra: extra,
+    );
+    return ToggleFavOnModel.fromMap(response);
+  }
+}
