97e8ac06b15b270eaef4423cfd9c8582f7f6c9ab
C0de871 <muhammadaydi3@gmail.com>
2024-12-27 20:47:22 +0300

    copy with for product entity

--- lib/features/products/domain/entities/product_enitty.dart
+++ lib/features/products/domain/entities/product_enitty.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class ProductEntity {
   int storeId;
   String storeName;
@@ -20,4 +21,28 @@ class ProductEntity {
     required this.mainImageUrl,
     required this.isFavorite,
   });
+
+  ProductEntity copyWith({
+    int? storeId,
+    String? storeName,
+    int? productId,
+    String? productName,
+    String? price,
+    int? quantity,
+    String? description,
+    String? mainImageUrl,
+    int? isFavorite,
+  }) {
+    return ProductEntity(
+      storeId: storeId ?? this.storeId,
+      storeName: storeName ?? this.storeName,
+      productId: productId ?? this.productId,
+      productName: productName ?? this.productName,
+      price: price ?? this.price,
+      quantity: quantity ?? this.quantity,
+      description: description ?? this.description,
+      mainImageUrl: mainImageUrl ?? this.mainImageUrl,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
+  }
 }
