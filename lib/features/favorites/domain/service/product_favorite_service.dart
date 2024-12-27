617376bb4ed3194efc43b373661582fdd8670aad
C0de871 <muhammadaydi3@gmail.com>
2024-12-27 20:48:45 +0300

    ProductFavoriteService class

---
+++ lib/features/favorites/domain/service/product_favorite_service.dart
@@ -0,0 +1,15 @@
+import 'package:e_commerce/core/databases/params/params.dart';
+
+abstract class ProductFavoriteService {
+  void updateProductFavoriteStatus({required ToggleFavOnParams params, required int isFavorite});
+  Stream<ProductFavoriteUpdate> get favoriteUpdates;
+}
+
+//! class to store the id of the favorite that I want to update:
+class ProductFavoriteUpdate {
+  final String productId;
+  final int isFavorite;
+  final String storeID;
+
+  ProductFavoriteUpdate(this.productId, this.isFavorite, this.storeID);
+}
