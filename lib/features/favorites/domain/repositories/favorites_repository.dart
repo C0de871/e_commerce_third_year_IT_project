71bb4e0143efea937421ce0044664d8b01d28e2b
C0de871 <muhammadaydi3@gmail.com>
2024-12-28 19:14:43 +0300

    absract toggle off fav method in repo

--- lib/features/favorites/domain/repositories/favorites_repository.dart
+++ lib/features/favorites/domain/repositories/favorites_repository.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/core/databases/errors/failure.dart';
 import 'package:e_commerce/core/databases/params/params.dart';
-import '../entities/toggle_fav_on_entitiy.dart';
+import '../entities/toggle_fav_entitiy.dart';
 
 abstract class FavoritesRepository {
-  Future<Either<Failure, ToggleFavOnEntity>> getToggleFavOn({required ToggleFavOnParams params});
+  Future<Either<Failure, ToggleFavEntity>> getToggleFavOn({required ToggleFavParams params});
+  Future<Either<Failure, ToggleFavEntity>> getToggleFavOff({required ToggleFavParams params});
 }
