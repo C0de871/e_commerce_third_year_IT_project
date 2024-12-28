aca93fc6f8d55779240ea1d98234e341e0bc9abb
C0de871 <muhammadaydi3@gmail.com>
2024-12-28 19:12:22 +0300

    call the new methods for check user and first time and choose route

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:e_commerce/core/databases/cache/secure_storage_helper.dart';
 import 'package:e_commerce/core/databases/cache/shared_prefs_helper.dart';
 import 'package:e_commerce/core/utils/services/fire_base_service.dart';
 import 'package:e_commerce/core/utils/services/service_locator.dart';
 import 'package:flutter/material.dart';
 
 import 'core/app/app.dart';
+import 'core/helper/app_functions.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +14,7 @@ void main() async {
   await getIt<SharedPrefsHelper>().init();
   await FireBaseService.initializeApp();
   await FireBaseService().initNotifications();
+  await checkIfLoggedInUser();
+  await checkIfFirstTime();
   runApp(const MyApp());
 }
