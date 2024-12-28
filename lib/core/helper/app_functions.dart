80f71723266497ceb8f522bd75fb41b38502607c
C0de871 <muhammadaydi3@gmail.com>
2024-12-28 19:11:41 +0300

    function to check first time logged in and choose initial route

--- lib/core/helper/app_functions.dart
+++ lib/core/helper/app_functions.dart
@@ -1,12 +1,19 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:e_commerce/core/helper/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
-SystemUiOverlayStyle getSystemUiOverlayStyle(
-    Brightness currentBrightness, BuildContext context) {
+import '../Routes/app_routes.dart';
+import '../databases/api/end_points.dart';
+import '../databases/cache/secure_storage_helper.dart';
+import '../databases/cache/shared_prefs_helper.dart';
+import '../utils/constants/constant.dart';
+import '../utils/services/service_locator.dart';
+
+SystemUiOverlayStyle getSystemUiOverlayStyle(Brightness currentBrightness, BuildContext context) {
   return currentBrightness == Brightness.light
       ? SystemUiOverlayStyle.light.copyWith(
           systemNavigationBarColor: Theme.of(context).colorScheme.surface,
@@ -22,15 +29,13 @@ SystemUiOverlayStyle getSystemUiOverlayStyle(
 
 Future<XFile?> pickImage() async {
   final ImagePicker picker = ImagePicker();
-  final image = await picker.pickImage(
-      source: ImageSource.gallery); // or ImageSource.camera
+  final image = await picker.pickImage(source: ImageSource.gallery); // or ImageSource.camera
   return image;
 }
 
 Future uploadImageToApi(XFile? image) async {
   if (image == null) return null;
-  return MultipartFile.fromFile(image.path,
-      filename: image.path.split('/').last);
+  return MultipartFile.fromFile(image.path, filename: image.path.split('/').last);
 }
 
 class RouteObserverService extends NavigatorObserver {
@@ -57,5 +62,33 @@ class RouteObserverService extends NavigatorObserver {
     for (var route in routeHistory) {
       log(route);
     }
+  }
+}
+
+checkIfLoggedInUser() async {
+  String? userToken = await SecureStorageHelper().getData(key: CacheKey.accessToken);
+  log(userToken!);
+  if (!userToken.isNullOrEmpty()) {
+    isLoggedInUser = true;
+  } else {
+    isLoggedInUser = false;
+  }
+}
+
+checkIfFirstTime() async {
+  isFristTime = await getIt<SharedPrefsHelper>().getData(key: CacheKey.isFirstTime);
+  if (isFristTime == null) {
+    isFristTime = true;
+    await getIt<SharedPrefsHelper>().saveData(key: CacheKey.isFirstTime, value: isFristTime);
+  }
+}
+
+String chooseInitialRoute() {
+  if (isFristTime!) {
+    return AppRoutes.splashRoute;
+  } else if (isLoggedInUser) {
+    return AppRoutes.homeRoute;
+  } else {
+    return AppRoutes.loginRoute;
   }
 }
