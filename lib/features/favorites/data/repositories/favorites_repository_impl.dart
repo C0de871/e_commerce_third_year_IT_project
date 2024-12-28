411f2191fbddf7249231683cc11257a3f78213f2
C0de871 <muhammadaydi3@gmail.com>
2024-12-28 19:14:54 +0300

    impl of toggle fav off in repo

--- lib/features/favorites/data/repositories/favorites_repository_impl.dart
+++ lib/features/favorites/data/repositories/favorites_repository_impl.dart
@@ -4,7 +4,7 @@ import 'package:e_commerce/core/databases/errors/failure.dart';
 import '../../../../core/databases/connection/network_info.dart';
 import '../../../../core/databases/errors/expentions.dart';
 import '../../../../core/databases/params/params.dart';
-import '../../domain/entities/toggle_fav_on_entitiy.dart';
+import '../../domain/entities/toggle_fav_entitiy.dart';
 import '../../domain/repositories/favorites_repository.dart';
 import '../datasources/favorites_remote_data_source.dart';
 
@@ -13,12 +13,28 @@ class FavoritesRepositoryImpl extends FavoritesRepository {
   final FavoritesRemoteDataSource remoteDataSource;
   FavoritesRepositoryImpl({required this.remoteDataSource, required this.networkInfo});
   @override
-  Future<Either<Failure, ToggleFavOnEntity>> getToggleFavOn({required ToggleFavOnParams params}) async {
+  Future<Either<Failure, ToggleFavEntity>> getToggleFavOn({required ToggleFavParams params}) async {
     if (await networkInfo.isConnected!) {
       try {
         final remoteToggleFavOn = await remoteDataSource.getToggleFavOn(params);
 
         return Right(remoteToggleFavOn);
+      } on ServerException catch (e) {
+        return Left(Failure(errMessage: e.errorModel.errorMessage));
+      }
+    } else {
+      //TODO make this message adapt to app language:
+      return Left(Failure(errMessage: "There is no internet connnect"));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ToggleFavEntity>> getToggleFavOff({required ToggleFavParams params}) async {
+    if (await networkInfo.isConnected!) {
+      try {
+        final remoteToggleFavOff = await remoteDataSource.getToggleFavOff(params);
+
+        return Right(remoteToggleFavOff);
       } on ServerException catch (e) {
         return Left(Failure(errMessage: e.errorModel.errorMessage));
       }
