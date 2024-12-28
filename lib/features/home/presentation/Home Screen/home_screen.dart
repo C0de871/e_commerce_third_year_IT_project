79b6bfa317cae6252a4189a5ad0bb3c8e05cf793
C0de871 <muhammadaydi3@gmail.com>
2024-12-28 19:13:49 +0300

    refactors

--- lib/features/home/presentation/Home Screen/home_screen.dart
+++ lib/features/home/presentation/Home Screen/home_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:developer';
+
+import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/core/databases/cache/storage_helper.dart';
 import 'package:e_commerce/features/home/presentation/Home%20Screen/widgets/home_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/databases/cache/secure_storage_helper.dart';
 import '../../../../core/utils/constants/app_numbers.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'widgets/discount_banner.dart';
@@ -18,8 +23,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void logREfreshtoken() async {
+    final String refresh = (await SecureStorageHelper().getData(key: CacheKey.refreshToken))!;
+    log("refresh token is: {$refresh}");
+  }
+
   @override
   void initState() {
+    logREfreshtoken();
     super.initState();
   }
 
@@ -36,15 +47,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 const SizedBox(height: padding4 * 3),
                 const DiscountBanner(),
                 const SizedBox(height: padding4 * 7),
-                SectionTitle(
-                    text: AppLocalizations.of(context)!.specialForYou,
-                    press: () {}),
+                SectionTitle(text: AppLocalizations.of(context)!.specialForYou, press: () {}),
                 const SizedBox(height: padding4 * 5),
                 const PopularStores(),
                 const SizedBox(height: padding4 * 7),
-                SectionTitle(
-                    text: AppLocalizations.of(context)!.popularProduct,
-                    press: () {}),
+                SectionTitle(text: AppLocalizations.of(context)!.popularProduct, press: () {}),
                 const SizedBox(height: padding4 * 5),
                 const PopularProductList(),
                 const SizedBox(height: 100),
