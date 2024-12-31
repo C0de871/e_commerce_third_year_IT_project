fb0bf665adfdd16351235de85bff98e6b770264a
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-31 22:46:18 +0300

    finish model cart and trailer file to practice

---
+++ lib/features/cart/data/models/cart_model.dart
@@ -0,0 +1,14 @@
+import 'package:e_commerce/core/databases/api/end_points.dart';
+import 'package:e_commerce/features/cart/data/models/sub_cart/sub_cart_model.dart';
+import 'package:e_commerce/features/cart/domain/entites/cart_entity.dart';
+
+class CartModel extends CartEntity {
+  CartModel({required super.totalPrice, required super.data});
+
+  factory CartModel.fromJson(Map<String, dynamic> json) {
+    return CartModel(
+      totalPrice: json[ApiKey.totalPrice],
+      data: (json[ApiKey.data])?.map((item) => SubCartModel.fromJson(item)).toList(),
+    );
+  }
+}
