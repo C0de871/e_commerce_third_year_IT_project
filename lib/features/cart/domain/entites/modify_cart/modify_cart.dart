7bfc91e20f2315478e27389859be914f68f08a88
ManarKurdy11 <manarkurdy111@gmail.com>
2024-12-31 22:48:12 +0300

    finish cart model

---
+++ lib/features/cart/domain/entites/modify_cart/modify_cart.dart
@@ -0,0 +1,11 @@
+import 'package:collection/collection.dart';
+
+class ModifyCartEntity {
+  String? message;
+  int? storeId;
+  int? productId;
+  int? cartAmount;
+
+  ModifyCartEntity({this.message, this.storeId, this.productId, this.cartAmount});
+
+}
