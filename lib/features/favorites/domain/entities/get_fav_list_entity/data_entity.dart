2d4f9b6397e9f1f0778f72eb85d06a5e57991175
C0de871 <muhammadaydi3@gmail.com>
2025-01-05 19:26:44 +0300

    get fav list  enitities

---
+++ lib/features/favorites/domain/entities/get_fav_list_entity/data_entity.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class DataEntity {
+  int? storeId;
+  String? storeName;
+  int? productId;
+  String? productName;
+  int? categoryId;
+  String? categoryName;
+  String? price;
+  int? quantity;
+  String? description;
+  int? isFavorite;
+  String? mainImage;
+  DataEntity({
+    required this.storeId,
+    required this.storeName,
+    required this.productId,
+    required this.productName,
+    required this.categoryId,
+    required this.categoryName,
+    required this.price,
+    required this.quantity,
+    required this.description,
+    required this.isFavorite,
+    required this.mainImage,
+  });
+}
---
+++ lib/features/favorites/domain/entities/get_fav_list_entity/get_fav_list_entity.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'data_entity.dart';
+
+class GetFavListEntity {
+  List<DataEntity>? data;
+  GetFavListEntity({
+    required this.data,
+  });
+}
