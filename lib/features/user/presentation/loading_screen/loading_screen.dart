770423c1aa64391649235c6bea0c5409c31c51da
C0de871 <muhammadaydi3@gmail.com>
2025-01-07 13:42:16 +0300

    fix errors

--- lib/features/user/presentation/loading_screen/loading_screen.dart
+++ lib/features/user/presentation/loading_screen/loading_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/Routes/app_routes.dart';
 import '../cubit/check_first_launch/check_first_launch_cubit.dart';
 import '../cubit/get_last_user_cubit/get_last_user_cubit.dart';
+import 'package:lottie/lottie.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -24,33 +25,37 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     log("in the loading screen build method");
-    return MultiBlocListener(
-      listeners: [
-        BlocListener<CheckFirstLaunchCubit, CheckFirstLaunchState>(
-          listener: (context, state) async{
-            if (state is CheckFirstLaunchLoaded) {
-              if (state.isFirstLaunch) {
-                await Future.delayed(Duration(seconds: 5));
-                Navigator.pushReplacementNamed(context, AppRoutes.splashRoute);
-              } else {
-                context.read<GetLastUserCubit>().getLastUser();
+    return Scaffold(
+      body: MultiBlocListener(
+        listeners: [
+          BlocListener<CheckFirstLaunchCubit, CheckFirstLaunchState>(
+            listener: (context, state) async {
+              if (state is CheckFirstLaunchLoaded) {
+                if (state.isFirstLaunch) {
+                  await Future.delayed(Duration(seconds: 5));
+                  Navigator.pushReplacementNamed(context, AppRoutes.splashRoute);
+                } else {
+                  context.read<GetLastUserCubit>().getLastUser();
+                }
               }
-            }
-          },
+            },
+          ),
+          BlocListener<GetLastUserCubit, GetLastUserState>(
+            listener: (context, state) async {
+              if (state is UserLoaded) {
+                await Future.delayed(Duration(seconds: 5));
+                Navigator.pushReplacementNamed(context, AppRoutes.homeRoute);
+              } else if (state is NoUserFound) {
+                await Future.delayed(Duration(seconds: 5));
+                Navigator.pushReplacementNamed(context, AppRoutes.loginRoute);
+              }
+            },
+          ),
+        ],
+        child: Center(
+          child: Lottie.asset('assets/lottie/motor.json'),
         ),
-        BlocListener<GetLastUserCubit, GetLastUserState>(
-          listener: (context, state) async {
-            if (state is UserLoaded) {
-              await Future.delayed(Duration(seconds: 5));
-              Navigator.pushReplacementNamed(context, AppRoutes.homeRoute);
-            } else if (state is NoUserFound) {
-              await Future.delayed(Duration(seconds: 5));
-              Navigator.pushReplacementNamed(context, AppRoutes.loginRoute);
-            }
-          },
-        ),
-      ],
-      child: Placeholder(),
+      ),
     );
   }
 }
