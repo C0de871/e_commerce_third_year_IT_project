158e6841b8fbdf6587d1add82d7da95c9b9f3b6b
ManarKurdy11 <manarkurdy111@gmail.com>
2025-01-07 13:19:42 +0300

    add stream between cart cubit & modify cubit

---
+++ lib/features/cart/data/serevice/modify_cart_service.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:e_commerce/features/cart/domain/entites/cart_entity/sub_cart_entity.dart';
+
+class ModifyCartService {
+  ModifyCartService();
+
+  final _controller = StreamController<SubCartEntity>.broadcast();
+
+  Stream<SubCartEntity> get modifyUpdatesStream => _controller.stream;
+//add to stream
+  void modifyCartProducts({required SubCartEntity subCartEntity}) {
+    print("in stream: ${subCartEntity.quantity}");
+    _controller.add(subCartEntity);
+  }
+}
+
+class ModifyCartUpdate {
+  final String productId;
+  final String storeId;
+  final String amount;
+  final String message;
+
+  ModifyCartUpdate(
+      {required this.productId,
+      required this.storeId,
+      required this.amount,
+      required this.message});
+}
