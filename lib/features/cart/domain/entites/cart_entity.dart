3594f0a06867a51d9677c9ce58f6ae6387d0821d
ManarKurdy11 <manarkurdy111@gmail.com>
2025-01-13 21:58:17 +0300

    total to double

--- lib/features/cart/domain/entites/cart_entity.dart
+++ lib/features/cart/domain/entites/cart_entity.dart
@@ -3,12 +3,12 @@ import 'package:collection/collection.dart';
 import 'cart_entity/sub_cart_entity.dart';
 
 class CartEntity {
-  final int? totalPrice;
+  final double? totalPrice;
   final List<SubCartEntity>? data;
 
   CartEntity({this.totalPrice, this.data});
 
-  CartEntity copyWith({List<SubCartEntity>? data, int? totalPrice}) {
+  CartEntity copyWith({List<SubCartEntity>? data, double? totalPrice}) {
     return CartEntity(
       data: data ?? this.data,
       totalPrice: totalPrice ?? this.totalPrice,
