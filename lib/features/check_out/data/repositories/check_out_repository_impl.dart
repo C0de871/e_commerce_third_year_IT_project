2a540e701e1f4f455697ea7b05dc5d7eceec58db
ManarKurdy11 <manarkurdy111@gmail.com>
2025-01-13 21:52:10 +0300

    repo for check out

--- lib/features/check_out/data/repositories/check_out_repository_impl.dart
+++ lib/features/check_out/data/repositories/check_out_repository_impl.dart
@@ -11,18 +11,40 @@ import 'package:e_commerce/features/cart/domain/entites/cart_entity.dart';
 import 'package:e_commerce/features/cart/domain/entites/modify_cart/modify_cart.dart';
 import 'package:e_commerce/features/cart/domain/repository/cart_repository.dart';
 import 'package:e_commerce/features/cart/domain/usecases/get_cart.dart';
+import 'package:e_commerce/features/check_out/data/datasourses/check_out_remote_data_source.dart';
+import 'package:e_commerce/features/check_out/domain/entites/check_out_order/check_out_order_entity.dart';
 import 'package:e_commerce/features/check_out/domain/repository/check_out_repository.dart';
 import 'package:e_commerce/features/user/data/datasourses/user_local_data_source.dart';
 import 'package:e_commerce/features/user/data/datasourses/user_remote_data_source.dart';
 
 class CheckOutRepositoryImpl extends CheckOutRepository {
   final NetworkInfo networkInfo;
-  final CartRemoteDataSource remoteDataSource;
+  final CheckOutRemoteDataSource remoteDataSource;
 
   CheckOutRepositoryImpl({
     required this.networkInfo,
     required this.remoteDataSource,
   });
+  //! create order:
+
+  @override
+  Future<Either<Failure,CheckOutOrderEntity>> createOrder( {required  bodyJson
+  } ) async{
+if (await networkInfo.isConnected!) {
+      try {
+        final remoteCreateOrderCart = await remoteDataSource.createOrder(bodyJson: bodyJson);
+        return Right(remoteCreateOrderCart);
+      } on ServerException catch (e) {
+        return Left(Failure(errMessage: e.errorModel.errorMessage));
+      }
+    } else {
+      return Left(
+        Failure(
+          errMessage: remoteDataSource.noInternectConnection(),
+        ),
+      );
+    }
+  }
 // //! get cart:
 //   @override
 //   Future<Either<Failure, CartEntity>> getCart() async {
