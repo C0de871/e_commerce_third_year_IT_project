7191988982873e975ef14c258116e83776be1712
C0de871 <muhammadaydi3@gmail.com>
2025-01-15 17:32:21 +0300

    trash commit

---
+++ e_commerce/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Apps\\flutter_windows_3.13.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\moham\\AndroidStudioProjects\\flutter_projects\\third year project e commerce\\e_commerce" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Apps\\flutter_windows_3.13.6-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\moham\\AndroidStudioProjects\\flutter_projects\\third year project e commerce\\e_commerce"
+  "FLUTTER_ROOT=D:\\Apps\\flutter_windows_3.13.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\moham\\AndroidStudioProjects\\flutter_projects\\third year project e commerce\\e_commerce\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\moham\\AndroidStudioProjects\\flutter_projects\\third year project e commerce\\e_commerce"
+  "FLUTTER_TARGET=C:\\Users\\moham\\AndroidStudioProjects\\flutter_projects\\third year project e commerce\\e_commerce\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\moham\\AndroidStudioProjects\\flutter_projects\\third year project e commerce\\e_commerce\\.dart_tool\\package_config.json"
+)
