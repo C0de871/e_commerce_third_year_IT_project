2383376d577f3a5b250f905067b1013048c63c4d
ManarKurdy11 <manarkurdy111@gmail.com>
2025-01-19 11:39:08 +0300

    remove extra import

--- e_commerce/lib/core/shared/screens/Navigation_cubit/navigation_bar_cubit.dart
+++ e_commerce/lib/core/shared/screens/Navigation_cubit/navigation_bar_cubit.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NavigationBarCubit extends Cubit<int> {
--- e_commerce/lib/core/shared/widgets/animated_search_field/animated_search_field.dart
+++ e_commerce/lib/core/shared/widgets/animated_search_field/animated_search_field.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-
-
 class AnimSearchBar extends StatefulWidget {
   ///  width - double ,isRequired : Yes
   ///  textController - TextEditingController  ,isRequired : Yes
