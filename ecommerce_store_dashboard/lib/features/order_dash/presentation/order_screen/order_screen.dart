f7f3d37631d11479fd343a0205c5eb3582b5e7ef
ManarKurdy11 <manarkurdy111@gmail.com>
2025-01-26 23:10:07 +0300

    add consumer to screen

--- ecommerce_store_dashboard/lib/features/order_dash/presentation/order_screen/order_screen.dart
+++ ecommerce_store_dashboard/lib/features/order_dash/presentation/order_screen/order_screen.dart
@@ -1,9 +1,10 @@
+import 'dart:developer';
+
+import 'package:ecommerce_store_dashboard/features/order_dash/presentation/cubit/get_order_dash_cubit.dart';
+import 'package:ecommerce_store_dashboard/features/order_dash/presentation/cubit/get_order_dash_state.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
-
-void main() {
-  runApp(const OrderScreen());
-}
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
@@ -15,7 +16,25 @@ class OrderScreen extends StatelessWidget {
         title: const Text('Order Card Example'),
         centerTitle: true,
       ),
-      body: const OrderScreenBody(),
+      body: BlocConsumer<GetOrderDashCubit, GetOrderDashState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          if(state is GetOrderDashSuccess){
+            log("********success");
+          return const OrderScreenBody();}
+          else{
+            log("********failure");
+
+            return const Center(
+              child: Text(
+              "Waiting........."
+              
+              ),);
+          }
+        },
+      ),
     );
   }
 }
@@ -27,10 +46,8 @@ class OrderScreenBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // Top OrderCard as table header
         const OrderCardHeader(),
         const Divider(),
-        // List of cards as table rows
         Expanded(
           child: ListView.builder(
             padding: const EdgeInsets.all(8),
@@ -38,11 +55,11 @@ class OrderScreenBody extends StatelessWidget {
             itemBuilder: (context, index) {
               return OrderCard(
                 orderId: 'ID${index + 1}',
-                status: 'Pending',
+                number: '${index + 1}',
                 orderDate: '2023-01-26',
                 owner: 'John Doe',
                 totalPrice: '\$${(index + 1) * 10}',
-                number: '${index + 1}',
+                status: 'Pending',
               );
             },
           ),
@@ -57,11 +74,10 @@ class OrderCardHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
-    return Card(
+    return const Card(
       elevation: 4,
-      margin: const EdgeInsets.all(16),
-      child: const Padding(
+      margin: EdgeInsets.all(16),
+      child: Padding(
         padding: EdgeInsets.all(16),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -69,13 +85,6 @@ class OrderCardHeader extends StatelessWidget {
             Expanded(
               child: Text(
                 'Order ID',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 12, color: Colors.grey),
-              ),
-            ),
-            Expanded(
-              child: Text(
-                'Status',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 12, color: Colors.grey),
               ),
@@ -108,6 +117,13 @@ class OrderCardHeader extends StatelessWidget {
                 style: TextStyle(fontSize: 12, color: Colors.grey),
               ),
             ),
+            Expanded(
+              child: Text(
+                'Status',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ),
           ],
         ),
       ),
@@ -115,7 +131,7 @@ class OrderCardHeader extends StatelessWidget {
   }
 }
 
-class OrderCard extends StatelessWidget {
+class OrderCard extends StatefulWidget {
   final String orderId;
   final String status;
   final String orderDate;
@@ -134,6 +150,43 @@ class OrderCard extends StatelessWidget {
   });
 
   @override
+  _OrderCardState createState() => _OrderCardState();
+}
+
+class _OrderCardState extends State<OrderCard> {
+  late Map<String, dynamic> selectedValue;
+
+  final List<Map<String, dynamic>> items = [
+    {'value': 'Pending', 'color': Colors.red, 'borderColor': Colors.redAccent},
+    {
+      'value': 'Shipped',
+      'color': Colors.green,
+      'borderColor': Colors.greenAccent
+    },
+    {
+      'value': 'Delivered',
+      'color': Colors.blue,
+      'borderColor': Colors.blueAccent
+    },
+    {
+      'value': 'Canceled',
+      'color': Colors.orange,
+      'borderColor': Colors.orangeAccent
+    },
+    {
+      'value': 'Returned',
+      'color': Colors.purple,
+      'borderColor': Colors.purpleAccent
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    selectedValue = items.firstWhere((item) => item['value'] == widget.status);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 2,
@@ -145,61 +198,86 @@ class OrderCard extends StatelessWidget {
           children: [
             Expanded(
               child: Text(
-                orderId,
+                widget.orderId,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 14),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                widget.orderDate,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 14),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                widget.owner,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 14),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                widget.totalPrice,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 14),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                widget.number,
                 textAlign: TextAlign.center,
                 style: const TextStyle(fontSize: 14),
               ),
             ),
             Expanded(
               child: DropdownButtonHideUnderline(
-                child: DropdownButton2<String>(
-                  value: status,
-                  onChanged: (String? newValue) {},
-                  items: <String>['Pending', 'Pan', 'Pand', 'Pann', 'Pa']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
+                child: DropdownButton2<Map<String, dynamic>>(
+                  value: selectedValue,
+                  onChanged: (Map<String, dynamic>? newValue) {
+                    setState(() {
+                      selectedValue = newValue!;
+                    });
+                  },
+                  items: items.map((item) {
+                    return DropdownMenuItem<Map<String, dynamic>>(
+                      value: item,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 4),
+                        decoration: BoxDecoration(
+                          color:
+                              item['color'].withOpacity(0.3), // جعل اللون خافت
+                          border: Border.all(
+                              color: Colors.transparent,
+                              width: 0), // إزالة الحدود
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          item['value'],
+                          style: TextStyle(color: item['color']), // نص غامق
+                        ),
+                      ),
                     );
                   }).toList(),
-                  buttonDecoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(8),
+                  buttonStyleData: ButtonStyleData(
+                    height: 50,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent, // جعل الخلفية شفافة
+                      border: Border.all(
+                          color: Colors.transparent, width: 0), // إزالة الحدود
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   ),
-                  dropdownDecoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
+                  dropdownStyleData: DropdownStyleData(
+                    decoration: BoxDecoration(
+                      // color: Colors.transparent, // جعل الخلفية شفافة
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   ),
-                  dropdownElevation: 8,
                 ),
-              ),
-            ),
-            Expanded(
-              child: Text(
-                orderDate,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 14),
-              ),
-            ),
-            Expanded(
-              child: Text(
-                owner,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 14),
-              ),
-            ),
-            Expanded(
-              child: Text(
-                totalPrice,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 14),
-              ),
-            ),
-            Expanded(
-              child: Text(
-                number,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 14),
               ),
             ),
           ],
